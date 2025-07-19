89aed720b12e31d64268861ddec3e8bfaf6364f9
Dushyant Garg <dushyantgarg37@gmail.com>
2025-07-20 01:47:18 +0530

    Added JWT authentication

---
+++ src/main/java/com/example/Assignment/service/MyUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.example.Assignment.service;
+
+import com.example.Assignment.entity.UserPrincipal;
+import com.example.Assignment.entity.Users;
+import com.example.Assignment.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = userRepo.findByUsername(username);
+        if (user == null) {
+            System.out.println("User Not Found");
+            throw new UsernameNotFoundException("user not found");
+        }
+
+        return new UserPrincipal(user);
+    }
+}
